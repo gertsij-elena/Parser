11bed3802f71edaa1ad962a53e5b455af9f403e2
elena-gertsij <gertsij.elena@yandex.ua>
2017-10-27 17:49:26 +0300

    add Parse Metod

--- Parser/Parser/Controllers/HomeController.cs
+++ Parser/Parser/Controllers/HomeController.cs
@@ -5,14 +5,31 @@ using System.Web;
 using System.Web.Mvc;
 using CsQuery;
 using CsQuery.Mvc;
+using Parser.Models;
 
 namespace Parser.Controllers
 {
     public class HomeController : Controller
     {
+        DataContext _db = new DataContext();
+
         public ActionResult Index()
         {
             return View();
-        }       
+        } 
+        
+        [HttpPost]
+        public string Parse()
+        {
+            //try
+            //{
+            //  return "succesconnection";
+            //}
+            //catch
+            //{
+            //    return "failconnection";
+            //}
+            return "succesconnection";
+        }
     }
 }
